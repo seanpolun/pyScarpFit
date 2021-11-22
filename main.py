53e03cf739ce46f3e2fca49511e953ea5237672f
seanpolun <seanpolun@gmail.com>
2021-11-22 17:43:21 -0600

    Added mode to compute uncertainty on model parameters

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import uncertainties
 from sklearn import linear_model
 import geopandas as gpd
 import json
+import statsmodels.api as sm
 
 # def scarp_ss(x, H, D, b ):
 #     Q = H / D
@@ -84,6 +85,7 @@ def fit_prof_mid(x, z):
         x = x[I]
         z = z[I]
         z = np.flip(z)
+        x = x[-1] - np.flip(x)
     if x.shape[0] > 5000:
         samp_step = 100
         n_out_rows = (x.shape[0] // samp_step) + 1
@@ -120,6 +122,7 @@ def refine_b(x, z, midx, midz):
         x = x[I]
         z = z[I]
         z = np.flip(z)
+        x = x[-1] - np.flip(x)
     x1 = x - midx
     z1 = z - midz
     # bmin = b_guess - (b_guess * 1.5)
@@ -140,6 +143,7 @@ def dsp_scarp_identify(x, z):
     z = z[I]
     if z[0] > z[-1]:
         z = np.flip(z)
+        x = x[-1] - np.flip(x)
     x_max = np.floor(x.max())
     if x_max < 100:
         sample_dist = 0.5
@@ -161,22 +165,46 @@ def dsp_scarp_identify(x, z):
     x_lower = x_lower.reshape((-1, 1))
     z_upper = z_new[(z_slope < 0) & (z_new > opt_midz)] - opt_midz
     z_lower = z_new[(z_slope < 0) & (z_new < opt_midz)] - opt_midz
-    upper_model = linear_model.LinearRegression().fit(x_upper, z_upper)
-    lower_model = linear_model.LinearRegression().fit(x_lower, z_lower)
-    b1 = lower_model.coef_
-    b2 = upper_model.coef_
+    # upper_model = linear_model.LinearRegression().fit(x_upper, z_upper)
+    # lower_model = linear_model.LinearRegression().fit(x_lower, z_lower)
+    # b1 = lower_model.coef_
+    # b2 = upper_model.coef_
+    lower_ols_x = sm.add_constant(x_lower, prepend=False)
+    upper_ols_x = sm.add_constant(x_upper, prepend=False)
+    lower_model = sm.OLS(z_lower, lower_ols_x)
+    upper_model = sm.OLS(z_upper, upper_ols_x)
+
+    lower_results = lower_model.fit()
+    upper_results = upper_model.fit()
+    b1 = lower_results.params[0]
+    b2 = upper_results.params[0]
+    int1 = lower_results.params[1]
+    int2 = upper_results.params[1]
+    lower_SE = lower_results.HC0_se
+    upper_SE = upper_results.HC0_se
+    b1_se = lower_SE[0]
+    b2_se = upper_SE[0]
+    int1_se = lower_SE[1]
+    int2_se = upper_SE[1]
+
+    b1_out = uncertainties.ufloat(b1, b1_se)
+    b2_out = uncertainties.ufloat(b2, b2_se)
+    int_1_out = uncertainties.ufloat(int1, int1_se)
+    int_2_out = uncertainties.ufloat(int2, int2_se)
 
     x_lower_ind = (np.abs(x - opt_midx - x_lower.max())).argmin()
     x_upper_ind = (np.abs(x - opt_midx - x_upper.min())).argmin()
 
     x_real_lower = x[0:x_lower_ind] - opt_midx
     x_real_lower = x_real_lower.reshape((-1, 1))
+    x_real_lower = sm.add_constant(x_real_lower, prepend=False)
     x_real_upper = x[x_upper_ind:-1] - opt_midx
     x_real_upper = x_real_upper.reshape((-1, 1))
+    x_real_upper = sm.add_constant(x_real_upper, prepend=False)
     z_real_lower = z[0:x_lower_ind] - opt_midz
     z_real_upper = z[x_upper_ind:-1] - opt_midz
-    z_lower_reg = lower_model.predict(x_real_lower)
-    z_upper_reg = upper_model.predict(x_real_upper)
+    z_lower_reg = lower_results.get_prediction(x_real_lower).predicted_mean
+    z_upper_reg = upper_results.get_prediction(x_real_upper).predicted_mean
 
     lower_resid = np.mean(z_real_lower - z_lower_reg)
     upper_resid = np.mean(z_real_upper - z_upper_reg)
@@ -188,51 +216,53 @@ def dsp_scarp_identify(x, z):
         idx = (np.abs(z_new - opt_midz)).argmin()
         opt_midx = x_new[idx]
 
-
     # H is half-height. Difference between y-intercepts in a system centered on the scarp
-    opt_h = (upper_model.intercept_ - lower_model.intercept_) / 2
+    opt_h = (int_2_out - int_1_out) / 2
 
     # IF mid-distance of scarp is not equal to lower intercept, adjust so that midpoint is at actual midpoint
-    z_adj = opt_h - np.abs(lower_model.intercept_)
+    z_adj = opt_h.n - np.abs(int_1_out.n)
     if np.abs(z_adj) > 0.05:
         opt_midz = opt_midz + z_adj
         idx = (np.abs(z_new - opt_midz)).argmin()
         opt_midx = x_new[idx]
 
-    return opt_midx, opt_midz, b1, b2, opt_h
+    return opt_midx, opt_midz, b1_out, b2_out, opt_h
 
 
-def fit_1event(x, z, xmid, zmid, b1, b2, H_guess):
-    H_unc = 0.15
-    H_unc_abs = np.abs(H_guess * H_unc)
+def fit_1event(x, z, xmid, zmid, b, H_guess):
+    # H_unc = 0.15
+    # H_unc_abs = np.abs(H_guess * H_unc)
+    H_unc = H_guess.s / H_guess.n
     x1 = x - xmid
     z1 = z - zmid
     D_min = 0.1
     D_max = 750.
     D_step = 0.1
-    b = gen_b_from_two(x1, b1, b2)
+    # b = gen_b_from_two(x1, b1, b2)
     z1 = z1 - (x1 * b)
-    opt_d = grid_search_d(scarp_1e, D_min, D_max, D_step, x1, z1, H_guess)
+    opt_d = grid_search_d(scarp_1e, D_min, D_max, D_step, x1, z1, H_guess.n)
     D = uncertainties.ufloat(opt_d, (H_unc * opt_d))
-    H = uncertainties.ufloat(H_guess, H_unc_abs)
+    H = H_guess
+    # H = uncertainties.ufloat(H_guess, H_unc_abs)
     return H, D
 
 
-def fit_ss_uplift(x, z, xmid, zmid, b1, b2, H_guess):
-    H_unc = 0.15
-    H_unc_abs = np.abs(H_guess * H_unc)
+def fit_ss_uplift(x, z, xmid, zmid, b, H_guess):
+    # H_unc = 0.15
+    # H_unc_abs = np.abs(H_guess *
+    H_unc = H_guess.s / H_guess.n
     x1 = x - xmid
     z1 = z - zmid
     D_min = 0.1
     D_max = 5000
     D_step = 0.1
-    b = gen_b_from_two(x1, b1, b2)
+    # b = gen_b_from_two(x1, b1, b2)
     z1 = z1 - (x1 * b)
     # popt, pcov = curve_fit(scarp_ss, x1, z1, p0=guess, bounds=bound1)
     # (H, D) = uncertainties.correlated_values(popt, pcov)
-    opt_d = grid_search_d(scarp_ss, D_min, D_max, D_step, x1, z1, H_guess)
+    opt_d = grid_search_d(scarp_ss, D_min, D_max, D_step, x1, z1, H_guess.n)
     D = uncertainties.ufloat(opt_d, (H_unc * opt_d))
-    H = uncertainties.ufloat(H_guess, H_unc_abs)
+    H = H_guess
     return H, D
 
 
@@ -323,6 +353,7 @@ class Scarp:
         self.default_unc = 0.001
         self.aspect = 1
         self.name = name
+        self.num_sim = 1000
         x_step = 2.5
 
         I = np.argsort(x)
@@ -330,6 +361,7 @@ class Scarp:
         self.z = z[I]
         if z[0] > z[-1]:
             self.z = np.flip(self.z)
+            self.x = x[-1] - np.flip(x)
         x_max = np.floor(self.x.max())
         if x_max < 100:
             x_step = 0.25
@@ -338,9 +370,7 @@ class Scarp:
         interp_scarp = interpolate.interp1d(self.x, self.z)
         self.z_lowres = interp_scarp(self.x_lowres)
         if b_fit == 'dsp':
-            self.midx, self.midz, b1, b2, self.Hinit = dsp_scarp_identify(self.x, self.z)
-            self.b1 = uncertainties.ufloat(b1, self.default_unc)
-            self.b2 = uncertainties.ufloat(b2, self.default_unc)
+            self.midx, self.midz, self.b1, self.b2, self.Hinit = dsp_scarp_identify(self.x, self.z)
             # self.Hinit = 1.0
         else:
 
@@ -351,20 +381,65 @@ class Scarp:
             self.b1 = binit
             self.b2 = binit
         self.b = gen_b_from_two(self.x - self.midx, self.b1.n, self.b2.n)
-
+        self.b_sim = np.empty((self.x.shape[0], self.num_sim), dtype=float)
+        b1_sims = (self.b1.s * np.random.randn(self.num_sim)) + self.b1.n
+        b2_sims = (self.b2.s * np.random.randn(self.num_sim)) + self.b2.n
+        for i in range(self.num_sim):
+            b_n = gen_b_from_two(self.x - self.midx, b1_sims[i], b2_sims[i])
+            self.b_sim[:, i] = b_n
         self.H1 = None
         self.D1 = None
         self.Hs = None
         self.Ds = None
+        self.H1_sim = None
+        self.D1_sim = None
+        self.Hs_sim = None
+        self.Ds_sim = None
         self.active_fig = None
 
     def gen_1e(self):
-        self.H1, self.D1 = fit_1event(self.x, self.z, self.midx, self.midz, self.b1.n, self.b2.n, self.Hinit)
+        self.H1, self.D1 = fit_1event(self.x, self.z, self.midx, self.midz, self.b, self.Hinit)
+
+    def sim_1e(self):
+        H1_sim = np.empty(self.num_sim)
+        D1_sim = np.empty(self.num_sim)
+        x1 = self.x - self.midx
+        z1 = self.z - self.midz
+        D_min = 1
+        D_max = 750.
+        D_step = 1
+        H_init = (self.Hinit.s * np.random.randn(self.num_sim)) + self.Hinit.n
+        for i in range(self.num_sim):
+            b = self.b_sim[:, i]
+            z2 = z1 - (x1 * b)
+            opt_d = grid_search_d(scarp_1e, D_min, D_max, D_step, x1, z2, H_init[i])
+            H1_sim[i] = H_init[i]
+            D1_sim[i] = opt_d
+        self.H1_sim = uncertainties.ufloat(np.mean(H1_sim), np.std(H1_sim))
+        self.D1_sim = uncertainties.ufloat(np.mean(D1_sim), np.std(D1_sim))
 
     def gen_ss(self):
-        self.Hs, self.Ds = fit_ss_uplift(self.x, self.z, self.midx, self.midz, self.b1.n, self.b2.n, self.Hinit)
+        self.Hs, self.Ds = fit_ss_uplift(self.x, self.z, self.midx, self.midz, self.b, self.Hinit)
 
-    def plot_scarp(self, type):
+    def sim_ss(self):
+        Hs_sim = np.empty(self.num_sim)
+        Ds_sim = np.empty(self.num_sim)
+        x1 = self.x - self.midx
+        z1 = self.z - self.midz
+        D_min = 1
+        D_max = 5000
+        D_step = 1
+        H_init = (self.Hinit.s * np.random.randn(self.num_sim)) + self.Hinit.n
+        for i in range(self.num_sim):
+            b = self.b_sim[:, i]
+            z2 = z1 - (x1 * b)
+            opt_d = grid_search_d(scarp_1e, D_min, D_max, D_step, x1, z2, H_init[i])
+            Hs_sim[i] = H_init[i]
+            Ds_sim[i] = opt_d
+        self.Hs_sim = uncertainties.ufloat(np.mean(Hs_sim), np.std(Hs_sim))
+        self.Ds_sim = uncertainties.ufloat(np.mean(Ds_sim), np.std(Ds_sim))
+
+    def plot_scarp(self, type, unc=False):
         self.active_fig = plt.figure(figsize=[8.5, 5.5], dpi=300)
         ax = self.active_fig.add_subplot(111)
         x1 = self.x - self.midx
@@ -372,24 +447,37 @@ class Scarp:
         ax.plot(x1, z1, 'o', color='k')
         # H = self.Hinit
         if type == "se":
-            H = self.H1.nominal_value
-            D = self.D1.nominal_value
-            scarp_model = scarp_1e(x1, H, D) + (x1 * self.b)
+            if unc:
+                H = self.H1_sim
+                D = self.D1_sim
+            else:
+                H = self.H1
+                D = self.D1
+            scarp_model = scarp_1e(x1, H.n, D.n) + (x1 * self.b)
         elif type == "ss":
-            H = self.Hs.nominal_value
-            D = self.Ds.nominal_value
-            scarp_model = scarp_ss(x1, H, D) + (x1 * self.b)
+            if unc:
+                H = self.Hs_sim
+                D = self.Ds_sim
+            else:
+                H = self.Hs
+                D = self.Ds
+            scarp_model = scarp_ss(x1, H.n, D.n) + (x1 * self.b)
         else:
             raise ValueError("Undefined scarp type")
 
-        ax.plot(x1, init_geom(x1, H, self.b), '--', c='darkgray')
+        ax.plot(x1, init_geom(x1, H.n, self.b), '--', c='darkgray')
         ax.plot(x1, scarp_model, ':', c='red')
         ax.set_aspect(self.aspect)
 
         name_text = self.name + "\n"
-        D_text = "$\kappa t$ = {:.1f} [$m^2$] \n".format(D)
-        H_text = "H = {:.1f} [m] \n".format(H)
-        b_text = "Far-Field Slope = {:.2f} \n".format(np.mean([self.b1.n, self.b2.n]))
+        if unc:
+            D_text = "$\kappa t$ = {:.1f} ".format(D.n) + "+/- {:.1f}  [$m^2$]\n".format(D.s)
+            H_text = "H = {:.2f} ".format(H.n) + "+/- {:.2f} [m] \n".format(H.s)
+            b_text = "Far-Field Slope = {:.2f} \n".format(np.mean([self.b1.n, self.b2.n]))
+        else:
+            D_text = "$\kappa t$ = {:.1f} [$m^2$] \n".format(D.n)
+            H_text = "H = {:.2f} [m] \n".format(H.n)
+            b_text = "Far-Field Slope = {:.2f} \n".format(np.mean([self.b1.n, self.b2.n]))
         midx_text = "Mid X = {:.0f} [m] \n".format(self.midx)
         midz_text = "Mid Z =  {:.0f} [m]".format(self.midz)
         if type == "ss":
@@ -398,8 +486,12 @@ class Scarp:
             model_text = "Single Event Uplift \n "
         else:
             model_text = ""
+        if unc:
+            unc_text = "+/- 1-$\sigma$ \n"
+        else:
+            unc_text = ""
         # annot_text = model_text + H_text + D_text + b_text + midx_text + midz_text
-        annot_text = name_text + model_text + H_text + D_text + b_text
+        annot_text = name_text + model_text + H_text + D_text + unc_text + b_text
         ax.text(-0.1, -0.1, annot_text, transform=ax.transAxes, horizontalalignment='right', bbox=dict(facecolor='white',
                                                                                                       alpha=0.7))
         plt.show()
